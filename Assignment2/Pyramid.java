ef17c16ef81b5464bc258878597dc47cd1e10c59
Andrewshoo <andrewshoot@yandex.ru>
2021-02-17 20:38:33 +0300

    added explanations

--- Assignment2/Pyramid.java
+++ Assignment2/Pyramid.java
@@ -27,19 +27,21 @@ public class Pyramid extends GraphicsProgram {
 	
 	public void run() {
 		
-		double x = (getWidth() - (BRICK_WIDTH * BRICKS_IN_BASE))/2;
-		double y = getHeight() - BRICK_HEIGHT;
+		double x = (getWidth() - (BRICK_WIDTH * BRICKS_IN_BASE))/2; //centers x pos
+		double y = getHeight() - BRICK_HEIGHT; // starting from bottom of the window
 		double l = BRICKS_IN_BASE;
-		for(int i = 0; i < BRICKS_IN_BASE; i++){
-			for(int j = 0; j < l; j++){
 		
-				double k = (j * BRICK_WIDTH) + x;
+		for(int i = 0; i < BRICKS_IN_BASE; i++){//loop for y (vertical)
+			for(int j = 0; j < l; j++){         //loop for x (horizontal)
+		
+				double k = x + (j * BRICK_WIDTH);
 				double n = y - (i * BRICK_HEIGHT);
 				GRect bricks = new GRect (k, n, BRICK_WIDTH, BRICK_HEIGHT);
 				add(bricks);
 			}
-			x+=BRICK_WIDTH/2;
-			l--;	
+			
+			x+=BRICK_WIDTH/2; //shifts the x position by half a brick
+			l--;//minus the brick	
 		}
 	}
 }
